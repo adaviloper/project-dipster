57766e28d8e77bb03cd6c2d90e40483c8b18d1f5
Krish <anudeep_stylishstar@yahoo.com>
2017-11-18 20:25:35 -0600

    Anudeep's Code

---
+++ controllers/UnsharpMaskingNHighBoostingController.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+from skimage.exposure import rescale_intensity
+from view import View
+import sys
+fc = sys.path.append('controllers/FilteringController')
+from controllers import FilteringController
+
+class UnsharpMaskingNHighBoostingController:
+    image = None
+
+    def get_gaussian_low_pass_filter(self, shape):
+
+        (rows, cols) = shape
+        output = np.zeros(shape)
+        for row in range(rows):
+            for col in range(cols):
+                distance = self.get_distance(col, cols, row, rows)
+                output[row, col] = (pow(np.e, -(distance**2 / (2 * self.cutoff ** 2)))).real
+        return output
+
+    def unsharpMasking(self, image, params):
+
+        image_path = 'controllers/assets/images/' + params['image']
+
+        input_image = cv2.imread(image_path, 0)
+
+        smoothImg = self.get_gaussian_low_pass_filter(input_image)
+        return 0
