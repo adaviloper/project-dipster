e3062dba9a719917facb79037a91da6fe496c215
adrian <adavila.dev@gmail.com>
2017-11-19 14:11:04 -0600

    Updated params key for statisticalFilter

--- controllers/StatisticalOrderController.py
+++ controllers/StatisticalOrderController.py
@@ -10,7 +10,7 @@ class StatisticalOrderController:
         image_path = 'controllers/assets/images/' + params['image']
         windowsize = params['windowSize']
         windowsize = int(windowsize[-1])
-        filtertype = params['filter']
+        filtertype = params['statisticalFilter']
 
         input_image = cv2.imread(image_path, 0)
         a = StatisticalOrderController()
@@ -26,10 +26,10 @@ class StatisticalOrderController:
         image_output2_path = 'controllers/assets/images/out/2_' + params['image']
         cv2.imwrite(image_output1_path, out_img1)
         cv2.imwrite(image_output2_path, out_img2)
-        #str=image_output2_path+'?ssim=0.8'
-        #print(str)
+        # str=image_output2_path+'?ssim=0.8'
+        # print(str)
         view = View()
-        output = view.render(message = [ image_output1_path ,image_output2_path])
+        output = view.render(message = [image_output1_path, image_output2_path])
 
         return '200 okay', output
 
