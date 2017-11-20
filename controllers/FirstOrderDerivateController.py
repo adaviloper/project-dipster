fc368d0afaadb1f63e9b56ca707e8a46dc073fd4
Krish <anudeep_stylishstar@yahoo.com>
2017-11-20 02:13:23 -0600

    Routed output images to message array

--- controllers/FirstOrderDerivateController.py
+++ controllers/FirstOrderDerivateController.py
@@ -5,16 +5,22 @@ from view import View
 
 class FirstOrderDerivateController:
     def filter(self, params):
-        img = cv2.imread('dog.png', 0)
-        sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize = 5)
-        sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize = 5)
+        image_path = 'controllers/assets/images/' + params['image']
 
-        cv2.imshow("Original", img)
-        cv2.imshow("Sobel X", sobelx)
-        cv2.imshow("Sobel Y", sobely)
+        input_image = cv2.imread(image_path, 0)
 
-        cv2.waitKey(0)
+        window_size = int(params['windowSize'][0])
+        print("windowSize:", window_size)
+        
+        sobelx = cv2.Sobel(input_image, cv2.CV_64F, 1, 0, ksize = window_size)
+        sobely = cv2.Sobel(input_image, cv2.CV_64F, 0, 1, ksize = window_size)
+
+        sobelx_output_path = 'controllers/assets/images/out/sobelx_' + params['image']
+        sobely_output_path = 'controllers/assets/images/out/sobely_' + params['image']
+        
+        cv2.imwrite(sobelx_output_path, sobelx)
+        cv2.imwrite(sobely_output_path, sobely)
 
         view = View()
-        output = view.render(message = [])
+        output = view.render(message = [sobelx_output_path, sobely_output_path])
         return '200 okay', output
